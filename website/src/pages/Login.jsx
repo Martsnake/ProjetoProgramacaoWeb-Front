38d1dff9bfe5da1d2b8073b310949602623be5ca
bakamaki <puguinha.carmona@gmail.com>
2025-04-22 14:26:11 +0100

    css for login

--- website/src/pages/Login.jsx
+++ website/src/pages/Login.jsx
@@ -10,25 +10,30 @@ export default function Login() {
             <div className={styles.main}>
                 <div className={styles.container}>
                     <h1 className={styles.title}>Login</h1>
-                    <div ></div>
-                </div>
-                <div>
+
                     <div className={styles.input_group}>
                         <img src="" alt="" />
-                        <input type="text" placeholder='Inserir email ou numero de aluno' className={styles.input_field}/>
+                        <input
+                        type="text"
+                        placeholder="Inserir email ou numero de aluno"
+                        className={styles.input_field}
+                                />
                     </div>
-                </div>
-                <div >
+
                     <div className={styles.input_group}>
-                        <img src="" alt="" />
-                        <input type="password" placeholder='Inserir password' className={styles.input_field}/>
+                    <img src="" alt="" />
+                    <input
+                        type="password"
+                        placeholder="Inserir password"
+                        className={styles.input_field}
+                    />
                     </div>
-                </div>
-                <div>
+
                     <div className={styles.button_container}>
                         <button className={styles.login_button}>Login</button>
                     </div>
                 </div>
             </div>
+
     );
 }

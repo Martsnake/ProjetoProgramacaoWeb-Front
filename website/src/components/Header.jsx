c6199b2a4437dfb7c0a0dadd4a7b460fb621acd5
Martsnake <martimmalteztorres@gmail.com>
2025-05-12 15:50:32 +0100

    style: add user image

--- website/src/components/Header.jsx
+++ website/src/components/Header.jsx
@@ -3,18 +3,19 @@ import React, { useState } from "react";
 import styles from './Header.module.css';
 
 import logo from '../assets/Bitmap.svg'
+import user from '../assets/User.svg'
 
 export default function Header() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     
     return(
-        <header className={`${styles.header} ${isLoggedIn ? styles.loggedIn : ""}`}>
+        <header className={styles.header}>
             <img src={logo} alt="Iscte" className={styles.logo}/>
-            {/* Uma forma BASICA de modar o header quando o utilizador faz login(não vai ficar assim) */}
-            <div className={styles.logo}>Logo</div>
-            <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
-                {isLoggedIn ? "Logout" : "Login"}
-            </button>
+            
+            <div className={styles.logo}>
+                <img src={user} alt="User"/>
+            </div>
+           
         </header>
     );
 }

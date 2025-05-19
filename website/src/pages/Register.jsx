a4aa9c17417596edbea0276261a047bd0c63d56b
xVjVx <jaimedsm127@gmail.com>
2025-05-19 17:39:46 +0100

    feat: Update signup

--- website/src/pages/Register.jsx
+++ website/src/pages/Register.jsx
@@ -20,7 +20,7 @@ const Register = () => {
         setMessage(null);
           
         try {
-            const response = await fetch('http://localhost:8000/register', {
+            const response = await fetch('http://localhost:8000/signup', {
                 method: 'POST',
                 headers: {
                   'Content-Type': 'application/json'

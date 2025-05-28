83760c6f1f19b5713f370bdab98a674ae8ac322e
Martsnake <martimmalteztorres@gmail.com>
2025-05-28 21:10:43 +0100

    feat(eventsPage): -Made the layout of the page

--- website/src/pages/EventPage.jsx
+++ website/src/pages/EventPage.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import EventCard from "../components/EventCard.jsx";
 
 import styles from "./EventPage.module.css"
 
-export default function EventPage() {
+export default function EventPage({ event }) {
   return(
     <div className={styles.page}>
       <h1 className={styles.title}>Events</h1>
       <div className={styles.eventgrid}>
-        <div>Hello</div>
-        <div>Hello</div>
-        <div>Hello</div>
-        <div>Hello</div>
+        {events.map((event, index) => (
+          <EventCard
+            key={index}
+            title={event.title}
+            description={event.description}
+            date={event.date}
+          />
+        ))}
       </div>
     </div>
   );

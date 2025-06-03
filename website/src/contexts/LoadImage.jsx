fb8c82230a7ba9796dcee213138445e214b37bcd
DMarques <david.2005.miguel@gmail.com>
2025-06-03 16:18:28 +0100

    created LoadEventImage function

---
+++ website/src/contexts/LoadImage.jsx
@@ -0,0 +1,26 @@
+import {existsSync, readFileSync, createWriteStream} from "node:fs";
+import {get} from 'node:https';
+// imagesFolder = ../../public/images/
+
+// pfp_{username}.jpg
+// filename = event_{objectId}.jpeg/jpg/png/svg
+// dest = imagesFolder + filename
+const imagesFolder = "../../public/images"
+function LoadEventImage(event) {
+    let file;
+    const filePath = `${imagesFolder}/event_${event.id}.png`
+    if(!existsSync(filePath)) {
+        file = createWriteStream(imagesFolder);
+    let request = get(url, function (response) {
+            response.pipe(file);
+        });
+
+        request.on('error', function (err) {
+            console.log(err);
+        });
+    }
+    else {
+        file = readFileSync(filePath)
+    }
+    return URL.createObjectURL(file)
+}
